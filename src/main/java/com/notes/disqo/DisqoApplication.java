caf3409111c7b9055c7eacde9e24bb15fa171f3d
nareknaltakyan1 <nareknaltakyan1@gmail.com>
2021-06-12 14:11:10 +0400

    Initial commit

---
+++ src/main/java/com/notes/disqo/DisqoApplication.java
@@ -0,0 +1,13 @@
+package com.notes.disqo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisqoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisqoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notes/disqo/DisqoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notes.disqo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisqoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
